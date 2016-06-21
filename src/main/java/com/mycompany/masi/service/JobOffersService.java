d1731a686b0986e4a33aaeb0ae3f1557cd3ca508
Agnieszka Kubiś <207570@edu.p.lodz.pl>
2016-06-21 16:28:29 +0200

    praca nad selectem

--- src/main/java/com/mycompany/masi/service/JobOffersService.java
+++ src/main/java/com/mycompany/masi/service/JobOffersService.java
@@ -3,11 +3,13 @@ package com.mycompany.masi.service;
 import com.mycompany.masi.exception.JobOfferNotFoundException;
 import com.mycompany.masi.model.JobApplication;
 import com.mycompany.masi.model.JobOffer;
+import com.mycompany.masi.model.Skill;
 import com.mycompany.masi.repository.JobApplicationRepository;
 import com.mycompany.masi.repository.JobOffersRepository;
 import com.mycompany.masi.repository.SkillRepository;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -46,6 +48,24 @@ public class JobOffersService {
     public JobApplication jobApply(long userId, long jobOfferId, String userMessage) {
         JobApplication jobApplication = new JobApplication(jobOfferId, userId, userMessage);
         return jobApplicationRepository.save(jobApplication);
+    }
+
+    @PostConstruct
+    public void addSkills() {
+
+        Skill skill1 = new Skill(null, "JAVA", "PROGRAMOWANIE", 10, null, null);
+        Skill skill2 = new Skill(null, "C++", "PROGRAMOWANIE", 30, null, null);
+        Skill skill3 = new Skill(null, "praca w grupie", "MIĘKKIE", 20, null, null);
+        Skill skill4 = new Skill(null, "zarzadzanie projektami", "MIĘKKIE", 20, null, null);
+        Skill skill5 = new Skill(null, "angularJS", "PROGRAMOWANIE", 30, null, null);
+        Skill skill6 = new Skill(null, "NETBEANS", "PROGRAMOWANIE", 20, null, null);
+        skillRepository.save(skill1);
+        skillRepository.save(skill2);
+        skillRepository.save(skill3);
+        skillRepository.save(skill4);
+        skillRepository.save(skill5);
+        skillRepository.save(skill6);
+
     }
 
 }
